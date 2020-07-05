66b0e42fed33c4d4a2c0e096b4540ce5cae892f0
Facundo N. Lopez <facku.mail@gmail.com>
2020-07-05 09:08:36 -0300

    saque el lciente en la respuesta de las apl

--- src/models/application.model.js
+++ src/models/application.model.js
@@ -21,6 +21,7 @@ ApplicationSchema.options.toJSON = {
     transform: function (doc, ret, options) {
         ret.id = ret._id;
         delete ret._id;
+        delete ret.cliente;
         delete ret.__v;
         return ret;
     },
