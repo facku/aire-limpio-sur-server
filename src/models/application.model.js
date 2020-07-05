999c82f04411088fd9597457a87c2e6882b748cf
Facundo Lopez <facku.mail@gmail.com>
2020-07-05 18:19:13 -0300

    las aplicaciones deveulven el campo cliente

--- src/models/application.model.js
+++ src/models/application.model.js
@@ -21,7 +21,6 @@ ApplicationSchema.options.toJSON = {
     transform: function (doc, ret, options) {
         ret.id = ret._id;
         delete ret._id;
-        delete ret.cliente;
         delete ret.__v;
         return ret;
     },
